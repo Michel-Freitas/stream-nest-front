861d34ea68b7f3f6a46ca1ab3c516823fcb22173
Michel Freitas Santos <michel_freitas@virgo.atlantico.net.br>
2023-08-14 09:16:08 -0300

    Creating Project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Flex, Text } from "@chakra-ui/react";
+
+const App = () => {
+  return (
+    <Flex>
+      <Text fontSize="30" color="red">Ola</Text>
+    </Flex>
+  );
+};
+
+export default App;
